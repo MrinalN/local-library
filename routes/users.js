67f41f1c55ebd8bd3e52941aec3b529c9846f23f
MrinalN <78000916+MrinalN@users.noreply.github.com>
2021-05-31 21:33:20 -0700

    add comments to track users route flow

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
+//loads express module
 var express = require('express');
+//uses it to get .Router obj
 var router = express.Router();
 
+//specifies a route on the obj
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+
+
+  //"next" can be used in the future if you want to add multiple route handlers to "/"
 });
 
+//exports router from module (to be called in app.js)
 module.exports = router;
