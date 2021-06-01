7e97a73c2c6b8c03042e7139ec83dcd6ff4d02b7
MrinalN <78000916+MrinalN@users.noreply.github.com>
2021-05-31 21:26:12 -0700

    add comments to track flow of app.js

--- app.js
+++ app.js
@@ -4,21 +4,25 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//files called to handle related routes
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+//this is what is exported to www through module.exports
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+//add middleware libraries
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//now add the route handling code (required above) to the request handling chain
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
